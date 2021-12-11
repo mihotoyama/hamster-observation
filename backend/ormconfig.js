01edcd84ea70cdbb5f2419f98653188cf0eece60
Miho Toyama <tourspoitiers@gmail.com>
2021-12-11 16:34:16 +0900

    マイグレーションファイル検出できない問題を修正

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -3,7 +3,7 @@ module.exports = [
     type: 'sqlite',
     database: 'data/dev.sqlite',
     entities: ['dist/**/*.entity{.ts,.js}'],
-    migrations: ['dist/migrations/**/*.entity{.ts,.js}'],
+    migrations: ['dist/migrations/**/*.js'],
     cli: {
       entitiesDir: 'src/**/*.entity.ts',
       migrationsDir: 'migrations',
